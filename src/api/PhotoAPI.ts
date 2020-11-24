fbaa45a0c4019ec9cba09cd71270f0d8bcac4ccd
shtwangy <shtwangy@gmail.com>
2020-11-24 22:40:17 +0900

    feat(src/api/PhotoAPI): getPhoto method to return Image array.

--- src/api/PhotoAPI.ts
+++ src/api/PhotoAPI.ts
@@ -1,11 +1,19 @@
 import Image from "../types/image";
 
-export const getPhotos = async (query: string): Promise<any> => {
+export const getPhotos = async (query: string): Promise<Image[]> => {
     const ret = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
         .then(res => res.json())
         .then(data => {
-            console.log('get photos!!');
-            return data;
+            let images: Image[] = [];
+            return data.results.map((res: any) => {
+                const image: Image = {
+                    description: res.description,
+                    id: res.id,
+                    url: res.urls.raw
+                }
+                images.push(image);
+            });
+            return images;
         });
 
     return ret;

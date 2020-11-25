554173b5f771f8aff8e514f8d405380bcb35e41d
shtwangy <shtwangy@gmail.com>
2020-11-25 22:38:45 +0900

    feat(src/api/PhotoAPI): add alt_description to get value

--- src/api/PhotoAPI.ts
+++ src/api/PhotoAPI.ts
@@ -4,9 +4,11 @@ export const getPhotos = async (query: string): Promise<Photo[]> => {
     const ret = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
         .then(res => res.json())
         .then(data => {
+            console.log(data);
             let images: Photo[] = [];
             data.results.forEach((res: any) => {
                 const image: Photo = {
+                    alt_description: res.alt_description,
                     description: res.description,
                     id: res.id,
                     url: res.urls.raw

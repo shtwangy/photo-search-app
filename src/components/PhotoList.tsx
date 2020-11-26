e7dd448a03e3784b41abfb1d205c291a7b636beb
shtwangy <shtwangy@gmail.com>
2020-11-26 23:05:16 +0900

    fix(src/components/PhotoList.tsx): adjust layout

--- src/components/PhotoList.tsx
+++ src/components/PhotoList.tsx
@@ -1,4 +1,6 @@
 import React, {FC} from 'react';
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import Photo from "../types/Photo";
 import {makeStyles} from "@material-ui/core/styles";
 import {PhotoCard} from "./index";
@@ -7,26 +9,30 @@ interface Props {
     photos: Photo[]
 }
 
-const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        flexFlow: 'row wrap',
+const useStyles = makeStyles((theme) => ({
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
     },
-});
+}));
 
 const PhotoList: FC<Props> = (props: Props) => {
     const classes = useStyles();
     const photos = props.photos;
     return (
-        <div className={classes.root}>
-            {
-                photos.length > 0 && (
-                    photos.map(photo =>
-                        <PhotoCard key={photo.id} photo={photo}/>
+        <Container className={classes.cardGrid} maxWidth='md'>
+            <Grid container spacing={2}>
+                {
+                    photos.length > 0 && (
+                        photos.map(photo =>
+                            <Grid item key={photo.id} xs={12} sm={6} md={4}>
+                                <PhotoCard photo={photo}/>
+                            </Grid>
+                        )
                     )
-                )
-            }
-        </div>
+                }
+            </Grid>
+        </Container>
     );
 };
 

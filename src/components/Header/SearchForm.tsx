6b11e4e682739b9b43ece2b4cf2edd04d81e9e0e
shtwangy <shtwangy@gmail.com>
2020-11-27 00:22:26 +0900

    refactor(src/components/SearchForm): move to Header directory

--- src/components/Header/SearchForm.tsx
+++ src/components/Header/SearchForm.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
-import {TextInput} from "./UIKit";
+import {TextInput} from "../UIKit";
 import {makeStyles} from "@material-ui/core/styles";
 
 interface Props {
@@ -14,7 +14,7 @@ const useStyles = makeStyles({
     searchField: {
         alignItems: 'center',
         display: 'flex',
-        marginLeft: 32
+        marginLeft: 32,
     }
 });
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export {default as PhotoCard} from './PhotoCard';
 export {default as PhotoList} from './PhotoList';
-export {default as SearchForm} from './SearchForm';

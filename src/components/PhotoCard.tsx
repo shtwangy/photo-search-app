cbafb361c51aa8bda50eac7021de5b38d5a8b149
shtwangy <shtwangy@gmail.com>
2020-12-05 00:46:02 +0900

    feat(src/components/PhotoCard): display dummy chips
    Issues #8

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
+import Chip from '@material-ui/core/Chip'
 import Photo from '../types/Photo'
 
 interface Props {
@@ -23,13 +24,19 @@ const PhotoCard: FC<Props> = (props: Props) => {
   const classes = useStyles()
   const { photo } = props
   return (
-    <Card className={classes.card}>
-      <CardMedia
-        className={classes.media}
-        image={photo.url}
-        title={photo.altDescription}
-      />
-    </Card>
+    <div>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={photo.url}
+          title={photo.altDescription}
+        />
+      </Card>
+      <Chip label="Basic" />
+      <Chip label="Basic" />
+      <Chip label="Basic" />
+      <Chip label="Basic" />
+    </div>
   )
 }
 

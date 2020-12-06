02d6b3bf4da1d82025504799085448485b62fa26
shtwangy <shtwangy@gmail.com>
2020-12-06 21:37:14 +0900

    feat(src/App.tsx): scroll through the search results to see one after another
    Closes #9

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
 import usePhotoData from './uses/usePhotoData'
 import { PhotoList } from './components'
 import { Header } from './components/Header'
@@ -6,15 +7,28 @@ import { Header } from './components/Header'
 const App: FC = () => {
   const [text, setText] = useState('')
   const [query, setQuery] = useState('louvre')
+  const [page, setPage] = useState(1)
 
-  const photos = usePhotoData(query)
+  const photos = usePhotoData(query, page)
 
   return (
     <>
-      <Header text={text} setText={setText} setQuery={setQuery} />
-      <main>
-        <PhotoList photos={photos} />
-      </main>
+      <Header
+        text={text}
+        setText={setText}
+        setQuery={setQuery}
+        setPage={setPage}
+      />
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={() => setPage(page + 1)}
+        hasMore
+        initialLoad={false}
+      >
+        <main>
+          <PhotoList photos={photos} />
+        </main>
+      </InfiniteScroll>
     </>
   )
 }
--- src/uses/usePhotoData.ts
+++ src/uses/usePhotoData.ts
@@ -2,16 +2,22 @@ import { useState, useEffect } from 'react'
 import Photo from '../types/Photo'
 import getPhotos from '../api/PhotoAPI'
 
-const usePhotoData = (query: string): Photo[] => {
+const usePhotoData = (query: string, page: number): Photo[] => {
   const [photos, setPhotos] = useState([] as Photo[])
 
   useEffect(() => {
-    if (query) {
-      getPhotos(query).then((res) => {
-        setPhotos(res)
+    setPhotos([])
+  }, [query])
+
+  useEffect(() => {
+    if (query && page) {
+      getPhotos(query, page).then((res) => {
+        setPhotos((p: Photo[]): Photo[] => {
+          return [...p, ...res]
+        })
       })
     }
-  }, [query])
+  }, [query, page])
 
   return photos
 }

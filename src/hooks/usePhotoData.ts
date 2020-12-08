630dd65c281a8282c930ab2c30d40eb20cb679af
shtwangy <shtwangy@gmail.com>
2020-12-09 00:04:27 +0900

    feat(src/hooks/usePhotoData): return error

--- src/hooks/usePhotoData.ts
+++ src/hooks/usePhotoData.ts
@@ -2,24 +2,30 @@ import { useState, useEffect } from 'react'
 import Photo from '../types/Photo'
 import getPhotos from '../api/PhotoAPI'
 
-const usePhotoData = (query: string, page: number): Photo[] => {
+const usePhotoData = (
+  query: string,
+  page: number
+): { photos: Photo[]; error: boolean } => {
   const [photos, setPhotos] = useState([] as Photo[])
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     setPhotos([])
   }, [query])
 
   useEffect(() => {
-    if (query && page) {
-      getPhotos(query, page).then((res) => {
+    getPhotos(query, page)
+      .then((res) => {
         setPhotos((p: Photo[]): Photo[] => {
           return [...p, ...res]
         })
       })
-    }
+      .catch(() => {
+        setError(true)
+      })
   }, [query, page])
 
-  return photos
+  return { photos, error }
 }
 
 export default usePhotoData

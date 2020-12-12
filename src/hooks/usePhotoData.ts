4d62f6d45b533f33462ace59af84b7c32f338253
shtwangy <shtwangy@gmail.com>
2020-12-12 21:24:59 +0900

    feat(src/hooks/usePhotoData): add hasMore and loading to return value
    Issues #10

--- src/hooks/usePhotoData.ts
+++ src/hooks/usePhotoData.ts
@@ -2,30 +2,40 @@ import { useState, useEffect } from 'react'
 import Photo from '../types/Photo'
 import getPhotos from '../api/PhotoAPI'
 
-const usePhotoData = (
-  query: string,
-  page: number
-): { photos: Photo[]; error: boolean } => {
+interface usePhotoDataResponse {
+  photos: Photo[]
+  error: boolean
+  hasMore: boolean
+  loading: boolean
+}
+
+const usePhotoData = (query: string, page: number): usePhotoDataResponse => {
   const [photos, setPhotos] = useState([] as Photo[])
   const [error, setError] = useState(false)
+  const [hasMore, setHasMore] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     setPhotos([])
   }, [query])
 
   useEffect(() => {
+    setLoading(true)
+    setError(false)
     getPhotos(query, page)
       .then((res) => {
         setPhotos((p: Photo[]): Photo[] => {
           return [...p, ...res]
         })
+        setHasMore(res.length > 0)
+        setLoading(false)
       })
       .catch(() => {
         setError(true)
       })
   }, [query, page])
 
-  return { photos, error }
+  return { photos, error, hasMore, loading }
 }
 
 export default usePhotoData
